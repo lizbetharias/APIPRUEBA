28c25d0084c5da4e78afc35b22fafec64faca787
MINEDUCYT <MINEDUCYT@ALEXIA>
2024-09-12 00:09:26 -0600

    Agregar archivos de proyecto.

---
+++ APIPRUEBA/Models/BbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using APIPRUEBAS.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace APIPRUEBAS.Models
+{
+    public partial class BbContext : DbContext
+    {
+        public BbContext()
+        {
+        }
+
+        public BbContext(DbContextOptions<BbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; } = null!;
+        public virtual DbSet<Producto> Producto { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__CATEGORI__A3C02A10B08557C3");
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.IdProducto)
+                    .HasName("PK__PRODUCTO__09889210D9F18C32");
+
+                entity.HasOne(d => d.oCategoria)
+                    .WithMany(p => p.Producto)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .HasConstraintName("FK_IDCATEGORIA");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ APIPRUEBA/Models/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using APIPRUEBAS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace APIPRUEBAS.Models
+{
+    [Table("CATEGORIA")]
+    public partial class Categoria
+    {
+     
+        public Categoria()
+        {
+            Producto = new HashSet<Producto>();
+        }
+
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Descripcion { get; set; }
+
+        [InverseProperty("oCategoria")]
+        [JsonIgnore]
+        public virtual ICollection<Producto> Producto { get; set; }
+    }
+}
